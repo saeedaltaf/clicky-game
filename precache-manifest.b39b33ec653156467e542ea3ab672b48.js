2f90bee2a111e6d8d26fbcb5d9c5b620139ce72b
Saeed Altaf <saeedaltaf@gmail.com>
2019-10-12 14:59:26 -0400

    Updates

---
+++ precache-manifest.b39b33ec653156467e542ea3ab672b48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0154997706ea5072045cc310457db2c5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fe117bed04b40c037b0a",
+    "url": "/clicky-game/static/css/main.6a635057.chunk.css"
+  },
+  {
+    "revision": "dbec4d126cfa7bfd3b41",
+    "url": "/clicky-game/static/js/2.804caa16.chunk.js"
+  },
+  {
+    "revision": "fe117bed04b40c037b0a",
+    "url": "/clicky-game/static/js/main.d61f93b1.chunk.js"
+  },
+  {
+    "revision": "53ab38b72451e6a26cca",
+    "url": "/clicky-game/static/js/runtime-main.eca853f5.js"
+  }
+]);
